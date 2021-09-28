888f2c1984026db1d39b81af67a98642ea849469
George Moroz <agricolamz@gmail.com>
2021-09-28 15:14:44 +0300

    add table for Andic borrowing annotation

--- some_analyzing_scripts.R
+++ some_analyzing_scripts.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(tidytext)
 
 # get the phonological discription of every language ----------------------
-df <- read_csv("/home/agricolamz/work/databases/andic_dicts/andic_dicts.csv")
+df <- read_csv("andic_dicts.csv")
 
 df %>% 
   distinct(glottocode) %>% 
@@ -27,7 +27,8 @@ sound_list
 # creation table for borrowing annotation ---------------------------------
 
 df %>% 
-  select(lang, id_word, id_meaning, id, ipa, meaning_ru, bor, borrowing_source_word) %>% 
+  select(lang, id_word, id_meaning, id, ipa, meaning_ru, definition, bor, borrowing_source_word) %>% 
+  rename(borrowing_source_language = borrowing_source_word) %>% 
   mutate(borrowing_source_transcription = "") %>% 
   write_csv("andic_dicts_version_for_borrowings_annotation.csv", na = "")
 
