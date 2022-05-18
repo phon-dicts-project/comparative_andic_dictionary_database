a93f4dbcca34a49b4ed5994a1133e4d4a77b802e
George Moroz <agricolamz@gmail.com>
2022-05-18 14:42:13 +0300

    update phonetics

--- some_analyzing_scripts.R
+++ some_analyzing_scripts.R
@@ -25,6 +25,22 @@ df %>%
 sound_list
 
 df %>% 
+  mutate(ipa = str_replace_all(ipa, "eˌ", "ˌe"),
+         ipa = str_replace_all(ipa, "aːˌ", "ˌaː"),
+         ipa = str_replace_all(ipa, "iˌ", "ˌi"),
+         ipa = str_replace_all(ipa, "ĩː",  "ĩː"),
+         ipa = str_replace_all(ipa, "ĩː", "ĩː"),
+         ipa = str_replace_all(ipa, "oˌ", "ˌo"),
+         ipa = str_replace_all(ipa, "ũ", "ũ"),
+         ipa = str_replace_all(ipa, "ũ", "ũ"),
+         ipa = str_replace_all(ipa, "uˌ", "ˌu"),
+         ipa = str_replace_all(ipa, "õ", "о̃"),
+         ipa = str_replace_all(ipa, "õː","о̃ː" ),
+         ipa = str_replace_all(ipa, "aˌ", "ˌa")) %>% 
+  write_csv("andic_dicts.csv", na = "")
+  
+
+df %>% 
   select(glottocode, reference, ipa) %>% 
   unnest_tokens(sound, ipa, token = stringr::str_split, pattern = "-") 
 
@@ -33,24 +49,18 @@ library(tidyverse)
 df <- read_csv("andic_dicts.csv")
 
 df %>% 
-  distinct(glottocode) %>% 
-  mutate(language = lingtypology::lang.gltc(glottocode)) ->
-  languages
-
-df %>% 
-  left_join(languages) %>% 
   mutate(languages_source = str_c(language, " (", reference, ")")) %>% 
   filter(!is.na(meaning_ru)) %>% 
-  select(languages_source, lemma, meaning_ru, borrowing_source_language, definition) %>% 
+  select(languages_source, ipa, meaning_ru, borrowing_source_language, definition) %>% 
   mutate(borrowing_source_language = ifelse(is.na(borrowing_source_language),
                                             "native",
                                             borrowing_source_language)) %>% 
-  select(languages_source, lemma, meaning_ru, borrowing_source_language) %>% 
+  select(languages_source, ipa, meaning_ru, borrowing_source_language) %>% 
   group_by(meaning_ru, languages_source) %>%
   mutate(id = 1:n()) %>% 
   sample_n(1) %>% 
-  pivot_longer(names_to = "names", values_to = "value",  c(lemma, borrowing_source_language)) %>% 
-  mutate(languages_source = ifelse(names != "lemma", str_c(languages_source, " bor"), languages_source)) %>%
+  pivot_longer(names_to = "names", values_to = "value",  c(ipa, borrowing_source_language)) %>% 
+  mutate(languages_source = ifelse(names != "ipa", str_c(languages_source, " bor"), languages_source)) %>%
   select(-names) %>% 
   pivot_wider(names_from = languages_source, values_from = value) ->
   result
